3668334304a7dc71a9663fa7e17b95387e48fc7f
Induwara <induwara2k@gmail.com>
2024-04-21 11:13:47 +0530

    keyword explanation

--- Join-IsAlive/src/Main.java
+++ Join-IsAlive/src/Main.java
@@ -35,8 +35,19 @@ public class Main {
 
         t2.start();
 
+        System.out.println(t1.isAlive());
+        System.out.println(t2.isAlive());
+
+
         t1.join();
         t2.join();
+
+        //Join keyword is used to tell the current thread to wait until selected threads( .join threads ) to complete before continue and  go to next step
+        //isAlive is used to check whether to check th corresponding thread is running or not
+
+
+        System.out.println(t1.isAlive());
+        System.out.println(t2.isAlive());
 
         System.out.println("End");
 
