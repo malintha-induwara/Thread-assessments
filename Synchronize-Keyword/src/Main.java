390080840add979f08734b4c2455a43ede34315a
Induwara <induwara2k@gmail.com>
2024-04-21 11:22:52 +0530

    Synchroniz-keyword module added

---
+++ Synchronize-Keyword/src/Main.java
@@ -0,0 +1,21 @@
+class  Calculation{
+    int num;
+    public void increment(){
+        num++;
+    }
+}
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculation cal = new Calculation();
+
+
+
+
+
+    }
+}
