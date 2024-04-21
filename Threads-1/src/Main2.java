186b81f406c969817aa974392c0fefdaa0336ecd
Induwara <induwara2k@gmail.com>
2024-04-21 09:48:54 +0530

    runnable implement

--- Threads-1/src/Main2.java
+++ Threads-1/src/Main2.java
@@ -1,5 +1,22 @@
+class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Thread is Running");
+    }
+}
+
+
+
+
+
 public class Main2 {
     public static void main(String[] args) {
+
+
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.start();
 
     }
 }
