26a5d3b6c59c43f7a8eb217033da24464c073742
Induwara <induwara2k@gmail.com>
2024-04-21 11:35:45 +0530

    content update

--- Synchronize-Keyword/src/Main.java
+++ Synchronize-Keyword/src/Main.java
@@ -9,28 +9,20 @@ class  Calculation{
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Calculation cal = new Calculation();
 
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 1000; i++) {
-
-                if (cal.num<2000) {
-                    cal.increment();
-                    System.out.println(cal.num);
-                }
+                cal.increment();
             }
         });
 
 
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 1000; i++) {
-                if (cal.num<2000) {
-                    cal.increment();
-                    System.out.println(cal.num);
-                }
-
+                cal.increment();
             }
         });
 
@@ -38,7 +30,11 @@ public class Main {
         t1.start();
         t2.start();
 
+        t1.join();
+        t2.join();
 
+
+        System.out.println(cal.num);
 
 
     }
