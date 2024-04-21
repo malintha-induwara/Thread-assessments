97b2c067527cbac3fd3337aab6881ee234c8fbf0
Induwara <induwara2k@gmail.com>
2024-04-21 10:05:46 +0530

    thread 2 added

---
+++ Threads-2/src/Main.java
@@ -0,0 +1,44 @@
+class One extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Thread 1 Count "+ (i+1) );
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class Two extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Thread 2 Count "+ (i+1) );
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Thread one = new One();
+        Thread two = new Two();
+
+        one.start();
+        two.start();
+
+
+
+
+
+    }
+}
